98366ecce09dfe1797ae1dc87b9b51275110af6b
vishnu662607 <reddygopal4917@gmail.com>
2026-04-04 13:40:58 +0530

    Fixed port issue for Render

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,33 @@
-const express=require("express");
-const cors=require("cors");
-const connectDB=require("./config/db");
-const projectRoutes=require("./routes/projectRoutes");
-const skillRoutes = require("./routes/skillRoutes")
-const messageRoutes = require("./routes/messageRoutes")
-const authRoutes=require("./routes/authRoutes")
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const projectRoutes = require("./routes/projectRoutes");
+const skillRoutes = require("./routes/skillRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const authRoutes = require("./routes/authRoutes");
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const app = express();
 
-
-
-const app=express();
 app.use(express.json());
 app.use(cors());
-app.use("/api/projects",projectRoutes);
-app.use("/api/skills",skillRoutes);
-app.use("/api/contact",messageRoutes);
-app.use("/api",authRoutes);
 
-app.get('/',(req,res)=>{
-    res.send("Portfolio API running");
+app.use("/api/projects", projectRoutes);
+app.use("/api/skills", skillRoutes);
+app.use("/api/contact", messageRoutes);
+app.use("/api", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Portfolio API running");
 });
-const startServer=async()=>{
-    await connectDB()
-    app.listen(5000,()=>{
-    console.log("Server is running on port 5000");
-})
 
-}
-startServer()
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(PORT, () => {   
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+startServer();
