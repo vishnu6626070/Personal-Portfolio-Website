c731a87c602a3393d4c42fa13287340f500f9b12
vishnu662607 <reddygopal4917@gmail.com>
2026-04-04 17:55:46 +0530

    Updated DB connection to MongoDB Atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,17 @@
-const mongoose=require("mongoose")
-const connectDB=async()=>{
-    try{
-        await mongoose.connect("mongodb://127.0.0.1:27017/portfolio")
-        console.log("mongoDB connected")
+const mongoose = require("mongoose");
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-    }catch(error){
-        console.log("error")
-        process.exit(1)
-    }
-}
-module.exports=connectDB
+    console.log("MongoDB Atlas connected ✅");
+  } catch (error) {
+    console.error("MongoDB connection error ❌:", error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
